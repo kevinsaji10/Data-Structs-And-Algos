71a82b9be60da73861b3ef3f6ea8e3886e1bfa1a
Kevin <kevinsaji@outlook.sg>
2023-08-17 13:18:34 +0800

    added learning folder

---
+++ Week 1 ICE/Learning/Test.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Test {
+    public static void main(String[] args) {
+        List<String> result = generateData();
+        System.out.println(result);
+    }
+    // try and use abstract data type as return value where possible
+    public static List<String> generateData() {
+        List<String> result = new LinkedList<>();
+        
+        return result;
+        
+
+    }
+}
